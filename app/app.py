367866d464e1fce86c4a11ba8b9ff917e29eed07
dhruvshah1996 <dbs28@njit.edu>
2021-12-14 20:00:46 -0500

    Added the 4 articles from project 2

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 """A simple flask web app"""
 from flask import Flask
-from app.controllers.index_controller import IndexController
+from app.controllers.index_controller import IndexController, pylintController, AAAtestingController
+from app.controllers.index_controller import SOLIDController, OOPController
 from app.controllers.calculator_controller import CalculatorController
 
 app = Flask(__name__)
@@ -17,3 +18,19 @@ def calculator_get():
 @app.route("/calculator", methods=['POST'])
 def calculator_post():
     return CalculatorController.post()
+
+@app.route("/pylint", methods=['GET'])
+def pylint_get():
+    return pylintController.get()
+
+@app.route("/AAATest", methods=['GET'])
+def AAA_get():
+    return AAAtestingController.get()
+
+@app.route("/OOP", methods=['GET'])
+def OOP_get():
+    return OOPController.get()
+
+@app.route("/SOLID", methods=['GET'])
+def SOLID_get():
+    return SOLIDController.get()
