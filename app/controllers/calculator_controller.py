d13ca4921d39fc9b98780a2f772befcb190f2f27
dhruvshah1996 <dbs28@njit.edu>
2021-12-15 20:03:29 -0500

    im able to write to CSV

--- app/controllers/calculator_controller.py
+++ app/controllers/calculator_controller.py
@@ -1,18 +1,14 @@
-import pandas as pd
-
 from app.controllers.controller import ControllerBase
 from calc.calculator import Calculator
 from flask import render_template, request, flash, redirect, url_for
-
 
 class CalculatorController(ControllerBase):
     @staticmethod
     def post():
         if request.form['value1'] == '' or request.form['value2'] == '':
-            error = 'You have to enter a value for both fields please'
+            error = 'You must enter a value for value 1 and or value 2'
         else:
             flash('You successfully calculated')
-
             # get the values out of the form
             value1 = request.form['value1']
             value2 = request.form['value2']
@@ -23,15 +19,16 @@ class CalculatorController(ControllerBase):
             getattr(Calculator, operation)(my_tuple)
             result = str(Calculator.get_last_result_value())
 
-            d = {"value1": value1, "value2": value2, "operation": operation, "result": result}
-            df = pd.DataFrame(d)
-            df.to_csv("results.csv", index=None, na_rep="na", mode='a')
+            f = open("results.csv", "a")
+            f.write(f"{value1},{value2},{operation},{result}")
+            f.close()
 
             return render_template('result.html', value1=value1, value2=value2, operation=operation, result=result)
         return render_template('calculator2.html', error=error)
     @staticmethod
     def get():
         return render_template('calculator2.html')
+
 
 
 
