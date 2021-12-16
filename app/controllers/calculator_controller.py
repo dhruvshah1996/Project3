9a375dfb7ade5c225befd3d0512ef654e1408fb6
dhruvshah1996 <dbs28@njit.edu>
2021-12-15 22:26:25 -0500

    got the csv file to render to the results page

--- app/controllers/calculator_controller.py
+++ app/controllers/calculator_controller.py
@@ -1,16 +1,17 @@
+import os.path
 from app.controllers.controller import ControllerBase
 from calc.calculator import Calculator
 from flask import render_template, request, flash, redirect, url_for
-import os.path
-import pandas as pd
+
 
 class CalculatorController(ControllerBase):
     @staticmethod
     def post():
         if request.form['value1'] == '' or request.form['value2'] == '':
-            error = 'You must enter a value for value 1 and or value 2'
+            error = 'You have to enter a value for both fields please'
         else:
             flash('You successfully calculated')
+
             # get the values out of the form
             value1 = request.form['value1']
             value2 = request.form['value2']
@@ -30,15 +31,12 @@ class CalculatorController(ControllerBase):
             f1.write(f"{value1},{value2},{operation},{result}\n")
             f1.close()
 
-            df = pd.read_csv("results.csv")
-            print(df)
-
-            items = []
-            for row in df.iterrows():
-                items.append(row)
-
-            return render_template('result.html', items=items)
+            return render_template('result.html', value1=value1, value2=value2, operation=operation, result=result)
         return render_template('calculator2.html', error=error)
     @staticmethod
     def get():
         return render_template('calculator2.html')
+
+
+
+
